180bc4f25a53facc9e9dd39a86632d84ca1fe9b3
Stamenov96 <stefan.stamenov1996@gmail.com>
2014-06-18 22:28:06 +0300

    insertshiftuglyway

--- randomhex.rb
+++ randomhex.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 require 'securerandom'
 require 'prawn'
 
@@ -15,9 +16,12 @@ Prawn::Document.generate "hellohex.pdf" do |pdf|
   pdf.text "int a = orig | (insert<<8)"
   
 Prawn::Document.generate("test.pdf") do |pdf|
-    table_data = [["a=?\n int orig = #{a}\n int insert = #{b}\n int a = orig | (insert<<8)", "b=?\n int orig = #{a}\n int insert = #{b}\n int a = orig | (insert<<6)"]]
-
+    table_data = [["a=?\n int orig = #{a}\n int insert = #{b}\n int a = orig | (insert      8)", "b=?\n int orig = #{a}\n int insert = #{b}\n int a = orig | (insert      6)"]]
+	
     pdf.table(table_data, :width => 500, :cell_style => { :inline_format => true })
+	pdf.draw_text "<<", :at => [108,663]
+	pdf.draw_text "<<", :at => [358,663]
+	
 end
   
 end
