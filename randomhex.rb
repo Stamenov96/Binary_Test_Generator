28fdcfd133113ae5e62dccb16f16242545044ce9
Stamenov96 <stefan.stamenov1996@gmail.com>
2014-06-18 10:12:54 +0300

    my_first_task

--- randomhex.rb
+++ randomhex.rb
@@ -1,7 +1,15 @@
 require 'securerandom'
+require 'prawn'
+
 
 a=SecureRandom.hex(2)
-p a
 b=SecureRandom.hex(2)
-p b
 p a.hex.to_s(2)
+
+Prawn::Document.generate "hellohex.pdf" do |pdf|
+  pdf.text "a=?\n"
+  pdf.text "int orig = #{a}\n"
+  pdf.text "int insert = #{b}\n"
+  pdf.text "int a = orig | (insert<<8)"
+  
+end
