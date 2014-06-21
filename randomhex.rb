653eb27b90d9fe0eb46fd8b153fb0712fc97df39
Stamenov96 <stefan.stamenov1996@gmail.com>
2014-06-21 10:41:58 +0300

    <br>fix

--- randomhex.rb
+++ randomhex.rb
@@ -69,23 +69,23 @@ fileHtml.puts "<TD>OR=?\n int orig = #{a}<br> int insert = #{b}<br> int a = orig
 fileHtml.puts "</TR>"
 
 fileHtml.puts "<TR>"
-fileHtml.puts "<TD>OR=?\n int orig = #{a}\n int insert = #{b}\n int a = orig | (insert      7)\n int b = orig | (insert      5)\n int XOR = a ^ b;</TD>"
-fileHtml.puts "<TD>left=?\n int i=#{a}\n int left= #{a} | (1      10);</TD>"
+fileHtml.puts "<TD>OR=?<br> int orig = #{a}<br> int insert = #{b}<br> int a = orig | (insert      7)<br> int b = orig | (insert      5)<br> int XOR = a ^ b;</TD>"
+fileHtml.puts "<TD>left=?<br> int i=#{a}<br> int left= #{a} | (1      10);</TD>"
 fileHtml.puts "</TR>"
 
 fileHtml.puts "<TR>"
-fileHtml.puts "<TD>result=?\n long value1=#{c}\n long value2=#{d}\n int result=(value1      3)^(value2       2)</TD>"
-fileHtml.puts "<TD>result=?\n int value1=#{e}\n int value2=#{f}\n int result=(value1      3)^(value2      2)</TD>"
+fileHtml.puts "<TD>result=?<br> long value1=#{c}<br> long value2=#{d}<br> int result=(value1      3)^(value2       2)</TD>"
+fileHtml.puts "<TD>result=?<br> int value1=#{e}<br> int value2=#{f}<br> int result=(value1      3)^(value2      2)</TD>"
 fileHtml.puts "</TR>"
 
 fileHtml.puts "<TR>"
-fileHtml.puts "<TD>a=?\n long testValue=#{c}\n int a=0\n if (testValue & (1      4))\n {\n a=1\n }\n else\n {\n a=2\n }\n</TD>"
-fileHtml.puts "<TD>a=? result=?\n long testValue=#{c}\n int a=0\n int result=0\n if((result=testValue & testValue ^ testValue | (1      4)))\n {\n a=1\n }\n else\n {\n a=2\n }\n</TD>"
+fileHtml.puts "<TD>a=?<br> long testValue=#{c}<br> int a=0<br> if (testValue & (1      4))<br> {<br> a=1<br> }<br> else<br> {<br> a=2<br> }<br></TD>"
+fileHtml.puts "<TD>a=? result=?<br> long testValue=#{c}<br> int a=0<br> int result=0<br> if((result=testValue & testValue ^ testValue | (1      4)))<br> {<br> a=1<br> }<br> else<br> {<br> a=2<br> }<br></TD>"
 fileHtml.puts "</TR>"
 
 fileHtml.puts "<TR>"
-fileHtml.puts "<TD>result=?\n int value1=#{g}\n int value2=#{h}\n int result =(value1      3)^(value2      2)</TD>"
-fileHtml.puts "<TD>result=?\n int value1=#{i}\n int value2=#{j}\n int result =(value1      5)^(value2      4)</TD>"
+fileHtml.puts "<TD>result=?<br> int value1=#{g}<br> int value2=#{h}<br> int result =(value1      3)^(value2      2)</TD>"
+fileHtml.puts "<TD>result=?<br> int value1=#{i}<br> int value2=#{j}<br> int result =(value1      5)^(value2      4)</TD>"
 fileHtml.puts "</TR>"
 
 
